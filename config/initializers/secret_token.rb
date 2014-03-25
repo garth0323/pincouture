e125e498ac270423c9984648e417044e8524365e
Garth <garth0323@gmail.com>
2014-03-24 21:45:05 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pincouture::Application.config.secret_key_base = '8bac515d785a03e8a15097d820bc67f650b6543a48775cbdd0ed9deac8f47cad7205c65c6b8eaa5ec7795600b084c8bf4fe4883a455c8630bf35ff4c3522bd95'
