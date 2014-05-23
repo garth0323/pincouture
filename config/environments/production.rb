d23658e3ea4e93441ecebdcd735d0f7f6bf913cd
Garth <garth0323@gmail.com>
2014-05-22 21:17:14 -0700

    add ENV variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Pincouture::Application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['pincoutoure'],
+    :bucket => ENV['BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY'],
     :secret_access_key => ENV['AWS_SECRET_KEY']
     }
