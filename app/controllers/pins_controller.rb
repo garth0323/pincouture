649596f7ecaa40a6bd5fe3afb872bb77d3c4293f
Garth <garth0323@gmail.com>
2014-05-30 19:25:44 -0700

    update views

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,6 +4,7 @@ class PinsController < ApplicationController
 
   def index
     @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 25)
+    @celebrity = Celebrity.all
   end
 
   def show
